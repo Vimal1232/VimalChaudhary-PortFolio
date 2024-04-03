458b0062f591ddf931085fea4041857c4e5a2376
Vimal Chaudhary <86522515+Vimal1232@users.noreply.github.com>
2024-04-03 22:01:58 +0530

    front end change

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Contactform = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("https://server-nec1.onrender.com/client", {
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("error", error);
+      });
+  };
+
   return (
     <div>
       <div className="flex flex-col gap-6">
@@ -10,17 +35,21 @@ const Contactform = () => {
           </h1>
         </div>
         <div>
-          <form className="flex flex-col gap-6 w-full">
+          <form className="flex flex-col gap-6 w-full" onSubmit={handleSubmit}>
             <div className="flex gap-5 max-md:flex-wrap">
               <input
                 type="text"
                 placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 className=" p-4 rounded-xl bg-Primary text-[16px] text-white 
                 placeholder:text-Ter3 placeholder:text-[16px] outline-none flex-1 font-Inter font-light"
               />
               <input
                 type="email"
                 placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className=" p-4 rounded-xl bg-Primary text-[16px] text-white 
                 placeholder:text-Ter3 placeholder:text-[16px] outline-none flex-1 font-Inter font-light"
               />
@@ -30,12 +59,17 @@ const Contactform = () => {
                 name="text"
                 id="subject"
                 placeholder="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 className=" p-4 rounded-xl bg-Primary text-[16px] text-white 
                 placeholder:text-Ter3 placeholder:text-[16px] outline-none h-[200px] w-full font-Inter font-light "
               ></textarea>
             </div>
             <div>
-              <button className=" bg-Secondary text-Primary font-Inter font-bold p-4 rounded-xl w-full">
+              <button
+                type="submit"
+                className=" bg-Secondary text-Primary font-Inter font-bold p-4 rounded-xl w-full"
+              >
                 Send Message
               </button>
             </div>
